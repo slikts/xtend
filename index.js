fdea0112972cca85fbff99291a057d8412f68f24
slikts <dabas@untu.ms>
2014-04-18 19:23:42 +0300

    optimize loop

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = extend
 function extend() {
     var target = {}
 
-    for (var i = 0; i < arguments.length; i++) {
+    for (var i = 0, n = arguments.length; i < n; i++) {
         var source = arguments[i]
 
         for (var key in source) {
